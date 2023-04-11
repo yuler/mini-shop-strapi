c5e7db645b0055d0558c1599a9d73149912a2f32
yuler <is.yuler@gmail.com>
2023-04-11 22:23:04 +0800

    Fix `Product` controller generate descriptHTML field

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -1,20 +1,21 @@
-'use strict';
+'use strict'
 
-const markdownIt = require('markdown-it');
+const markdownIt = require('markdown-it')()
 
 /**
  * product controller
  */
-
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require('@strapi/strapi').factories
 
 module.exports = createCoreController('api::product.product', ({ strapi }) => ({
   async findOne(ctx) {
     // some custom logic here
     ctx.query = { ...ctx.query, local: 'en' }
     // Calling the default core action
-    const { data, meta } = await super.find(ctx);
-    data.attributes.descriptionHTML = markdownIt(data.attributes.description ?? '')
-    return { data, meta };
+    const { data, meta } = await super.findOne(ctx)
+    data.attributes.descriptionHTML = markdownIt.render(
+      data.attributes.description ?? ''
+    )
+    return { data, meta }
   },
-}));
+}))
