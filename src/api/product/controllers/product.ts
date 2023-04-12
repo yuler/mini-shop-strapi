9e547799f1c1b418f7ee30922c4099e7b700e0c8
yuler <is.yuler@gmail.com>
2023-04-12 18:09:46 +0800

    Move to typescript

---
+++ src/api/product/controllers/product.ts
@@ -0,0 +1,19 @@
+/**
+ * product controller
+ */
+
+import { factories } from '@strapi/strapi'
+import MarkdownIt from 'markdown-it'
+
+const markdownIt = new MarkdownIt()
+
+export default factories.createCoreController('api::product.product', ({ strapi }) => ({
+	async findOne(ctx) {
+		// some custom logic here
+		ctx.query = { ...ctx.query, local: 'en' }
+		// Calling the default core action
+		const { data, meta } = await super.findOne(ctx)
+		data.attributes.descriptionHTML = markdownIt.render(data.attributes.description ?? '')
+		return { data, meta }
+	},
+}))
